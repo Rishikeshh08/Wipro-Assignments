efe76255340fd66a91602d9c036b68779ffe7ce4
Rishikeshh08 <117388775+Rishikeshh08@users.noreply.github.com>
2025-12-08 04:23:26 +0530

    Delete employeeapp directory

--- employeeapp/src/main/java/com/example/employeeapp/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeeapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.employeeapp.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
